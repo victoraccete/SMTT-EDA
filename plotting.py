90a318d784e454d44d9c1f8d68b0fd031851311e
Victor Accete <victor.accete@gmail.com>
2021-02-12 22:17:11 -0300

    Adding preprocessing and plotting functions files

---
+++ plotting.py
@@ -0,0 +1,294 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+pd.options.mode.chained_assignment = None
+sns.set_style('white')
+
+weekday_map = {
+        # according to documentation: https://docs.python.org/3/library/datetime.html#datetime.datetime.weekday
+        0: 'Segunda-feira',
+        1: 'Terça-feira',
+        2: 'Quarta-feira', 
+        3: 'Quinta-feira', 
+        4: 'Sexta-feira', 
+        5: 'Sábado', 
+        6: 'Domingo'
+    }
+
+
+def plot_performed_vs_unperformed(performed: int, unperformed: int) -> None:
+    ''' Plota dois gráficos lado a lado para comprar viagens realizadas e não realizadas.
+        Um gráfico é um gráfico de barras, com os valores totais; e o outro gráfico é um 
+        gráfico de anel com os percentuais. 
+        
+        Args:
+            performed: Inteiro. Quantidade de viagens realizadas/cumpridas.
+            unperformed: Inteiro. Quantidade de viagens não realizadas/não cumpridas. 
+        Return:
+            None. Apenas mostra o gráfico.
+    '''
+
+    plt.figure(figsize=(12,6))
+
+    plt.subplot(1, 2, 1)
+    plt.bar(x=['Viagens realizadas', 'Viagens não realizadas'], height=[performed, unperformed], color='lightseagreen')
+    xlocs, _ = plt.xticks()
+    for i, value in enumerate([performed, unperformed]):
+        plt.text(x=xlocs[i], y=value-400, s=value, horizontalalignment='center', 
+                 verticalalignment='top', fontsize=16, color='snow', fontweight='bold')
+    plt.yticks([0, 10000, 20000])
+    plt.xticks(fontsize=12)
+    plt.box(False)
+    
+    plt.subplot(1, 2, 2)
+    plt.pie(labels=['Viagens realizadas', 'Viagens não realizadas'], x=[performed, unperformed], 
+            autopct='%1.2f%%', wedgeprops=dict(width=0.22), colors=['lightseagreen', 'lightcoral'])
+    plt.box(False)
+
+    plt.suptitle("Comparativo de viagens realizadas e não realizadas", fontsize=16)
+
+    plt.show()
+    return None
+
+
+def plot_performed_vs_unperformed_by_direction(df: pd.DataFrame, barmode="group") -> None:
+    '''Mostra um gráfico de barras interativo comparando as viagens realizadas e não realizadas,
+    mas mostrando separado por viagens de 'Ida' e 'Volta'.
+    Args:
+        df: DataFrame do pandas a ser usada. 
+        barmode: str. Referente ao modo como as barras serão apresentadas. Pode ser "group" (agrupado) ou "stack" (empilhado).
+    Return:
+        None. Apenas plota o gráfico.
+    '''
+    fig = px.histogram(data_frame=df, x='viagem_realizada', color='sentido_viagem', 
+                       barmode=barmode,  template='plotly_white', 
+                       color_discrete_sequence=['#4C78A8', '#00CC96'],
+                       )
+    fig.update_layout(
+            legend=dict(
+                title='Sentido da viagem',
+            ),
+            yaxis_title='Quantidade de viagens',
+            yaxis_showticklabels=True,
+            xaxis_title=''
+        )
+    
+    fig.update_traces(hovertemplate='%{y} viagens')
+
+    fig.show()
+    return
+
+def plot_performed_vs_unperformed_by_company(df: pd.DataFrame, barmode="group") -> None:
+    '''Mostra um gráfico de barras interativo comparando as viagens realizadas e 
+    não realizadas por empresa.
+    Args:
+        df: DataFrame do pandas a ser usada. 
+        barmode: str. Referente ao modo como as barras serão apresentadas. Pode ser "group" (agrupado) ou "stack" (empilhado).
+    Return:
+        None. Apenas plota o gráfico.
+    '''
+    if not (barmode == "stack" or barmode == "group"):
+        raise AssertionError("Modo de apresentação precisa ser 'stack' ou 'group'")
+    
+    fig = px.histogram(data_frame=df, 
+             x='apelido_empresa', 
+             color='viagem_realizada', 
+             barmode=barmode,
+             title='Viagens realizadas e não realizadas por empresa',
+             labels={
+                 'count': 'Quantidade de viagens',
+                 'apelido_empresa': 'Empresa',
+                 'viagem_realizada': 'Viagem'
+             },
+             template='plotly_white',
+             )
+    fig.update_layout(
+        legend=dict(
+            title='',
+        ),
+        yaxis_title='Quantidade de viagens',
+        yaxis_showticklabels=True,
+    )
+
+    fig.update_traces(hovertemplate='%{x}<br>%{y} viagens')
+    fig.show()
+
+    return None
+
+
+def plot_unperformed_frequency(df: pd. DataFrame, by="hour", kind="line"):
+    ''' Plota um gráfico interativo para apresentar frequência de viagens não realizadas. 
+        Pode ser por hora do dia ou por dia da semana e pode ser gráfico de linha ou de barra.
+        Args:
+            df: DataFrame do pandas usado como base.
+            by: Pode ser "hour" (hora do dia) ou "weekday" (dia da semana)
+            kind: Pode ser "line" (gráfico de linha) ou "bar" (gráfico de barra)
+        Return:
+            None. Apenas plota o gráfico.
+    '''
+    if by == "hour":
+        x = 'Hora prevista'
+        title = 'Média de viagens não realizadas por hora do dia'
+    elif by == "weekday":
+        x = 'Dia da semana'
+        title = 'Média de viagens não realizadas por dia da semana'
+    else:
+        raise AssertionError("Modo do parâmetro 'by' admite apenas 'hour' ou 'weekday'")
+    if kind == 'line':
+        fig = px.line(data_frame=df, x=x, y='Viagens não realizadas', 
+                  text='Viagens não realizadas', title=title, template='plotly_white')
+        fig.update_traces(textposition='top center')
+    elif kind == 'bar':
+        fig = px.bar(data_frame=df, x=x, y='Viagens não realizadas', 
+                  text='Viagens não realizadas', title=title, template='plotly_white')
+    else:
+        raise AssertionError("Modo do parâmetro 'kind' admite apenas 'line' ou 'bar'")
+
+    fig.show()
+    return None
+
+
+def plot_top_unperformed(df: pd.DataFrame) -> None:
+    ''' Plota um gráfico interativo com as linhas com mais viagens não realizadas,
+        com indicação de empresa responsável também.
+        Args:
+            df: DataFrame do Pandas a ser usada. 
+        Return:
+            None. Apenas plota o gráfico.
+    '''
+    fig = px.bar(data_frame=df, y='nome_linha', x='viagens não realizadas', orientation='h', color='apelido_empresa',
+                color_discrete_sequence=['#00CC96', '#636EFA', '#FECB52'], title="Linhas com mais viagens não realizadas", 
+                 template='plotly_white', text='viagens não realizadas'
+                )
+    fig.update_layout(
+                    legend=dict(
+                        title='Empresa responsável'
+                    ),
+                    yaxis_categoryorder='total ascending',
+                    yaxis_title='',
+                    xaxis_title='Quantidade de viagens não realizadas',
+                    )
+    fig.update_traces(textposition='outside')
+    fig.update_traces(hovertemplate='%{y}<br>%{x} viagens não realizadas')
+
+    fig.show()
+    return None
+
+def plot_mean_delay(df: pd.DataFrame) -> None:
+    ''' Plota um gráfico estático com o atraso médio por empresa, com uma linha mostrando
+        o atraso médio geral (entre todas as empresas).
+        Args:
+            df: DataFrame do pandas com os dados a serem plotados. 
+        Return:
+            None. Apenas mostra o gráfico. 
+    '''
+    mean_delay = df.groupby(['apelido_empresa'])[['tempo_de_atraso', 'apelido_empresa']].mean().reset_index().sort_values(by='tempo_de_atraso')
+
+    plt.figure(figsize=(14,8))
+    ax = sns.barplot(data=mean_delay, x='apelido_empresa', y='tempo_de_atraso', ci=None, color='mediumturquoise')
+    xlocs, _ = plt.xticks()
+    for i, value in enumerate(mean_delay['tempo_de_atraso']):
+        plt.text(x=xlocs[i], y=value-0.08, s=f'{value:.2f} min', horizontalalignment='center', 
+                verticalalignment='top', fontsize=16, color='snow', fontweight='bold')
+    plt.xticks(fontsize=12)
+    ax.set_xlabel(''), ax.set_ylabel('')
+    plt.title("Média de atraso por empresa (minutos)", fontdict={'size': 16, 'color': '#444'})
+    mean = mean_delay['tempo_de_atraso'].mean()
+    ax.plot([-0.55, 3.55], [mean]*2, label=f"Média geral: {mean:.2f} min", linestyle='--', color="firebrick", linewidth=2)
+    legend = ax.legend(loc='upper left', fontsize=12)
+    #legend.get_texts()[0].set_color("firebrick")
+    sns.despine(bottom=True, left=True)
+
+    plt.show()
+    return None
+
+
+def plot_mean_delay_by_direction(df: pd.DataFrame) -> None:
+    ''' Plota um gráfico interativo para visualizar o atraso médio, por empresa, e
+        por sentido da viagem (ida ou volta).
+        Args:
+            df: DataFrame do pandas com os dados a serem visualizados.
+        Return:
+            None. Apenas mostra o gráfico.
+    '''
+    mean_delay = df.groupby(['apelido_empresa', 'sentido_viagem'])[['tempo_de_atraso', 'apelido_empresa', 'sentido_viagem']].mean().reset_index().sort_values(by='tempo_de_atraso')
+    mean_delay['tempo_de_atraso'] = mean_delay['tempo_de_atraso'].round(2)
+    fig = px.bar(mean_delay, x='sentido_viagem', y='tempo_de_atraso', color='apelido_empresa', template='plotly_white', 
+                barmode='group', text='tempo_de_atraso', color_discrete_sequence=['#FECB52', '#EF553B', '#00CC96', '#636EFA'])
+    fig.update_layout(
+        legend=dict(
+            title='Empresa responsável'
+        ),
+        yaxis_title='Atraso médio (em minutos)',
+        xaxis_title='Sentido da viagem',
+    )
+    fig.update_traces(textposition='outside')
+    fig.show()
+    return None
+
+def plot_boxplot_delay(df: pd.DataFrame):
+    '''Plota um boxplot interativo com informações sobre os atrasos por empresa.
+        Args:
+            df: DataFrame do Pandas com informações a serem visualizadas. 
+        Return
+            None. Apenas mostra o gráfico
+    '''
+    fig = px.box(data_frame=df, y='tempo_de_atraso', color='apelido_empresa', template='plotly_white',
+                color_discrete_sequence=['#EF553B', '#00CC96', '#636EFA', '#FECB52'], title="Boxplot tempo de atraso por empresa")
+    fig.update_layout(
+                    legend=dict(
+                        title='Empresa responsável'),
+                    yaxis_title='Atraso (em minutos)',
+                    )
+    fig.show()
+    return None
+
+def plot_delay_info(df: pd.DataFrame) -> None:
+    ''' Plota um gráfico interativo com percentual de viagens atrasadas, adiantadas e pontuais
+    por empresa. 
+        Args:
+            df: DataFrame do Pandas com informações a serem visualizadas. 
+        Return:
+            None. Apenas plota o gráfico.
+    '''   
+    fig = px.bar(data_frame=df, x='Empresa', y='Percentual', color='Status', barmode='group', text='Percentual',
+                color_discrete_sequence=['#636EFA', '#D62728', '#1C8356'], template='plotly_white', 
+                title='Percentual de viagens atrasadas, adiantadas e pontuais')
+    fig.update_layout(
+                    legend=dict(
+                        title=''),
+                    yaxis_title='Percentual de ocorrência (em %)',
+                    )
+    fig.show()
+    return None
+
+
+def plot_top_delayed(df: pd.DataFrame) -> None:
+    ''' Plota um gráfico interativo com as linhas com maior percentual de atraso,
+        com indicação de empresa responsável também.
+        Args:
+            df: DataFrame do Pandas a ser usada. 
+        Return:
+            None. Apenas plota o gráfico.
+    '''
+    fig = px.bar(data_frame=df, y='nome_linha', x='Percentual de viagens com atraso', orientation='h', color='apelido_empresa',
+                color_discrete_sequence=['#FECB52', '#636EFA', '#00CC96', '#EF553B'], 
+                 title="10 linhas com maior percentual de viagens atrasadas", template='plotly_white',
+                 text='Percentual de viagens com atraso'
+                )
+    fig.update_layout(
+                    legend=dict(
+                        title='Empresa responsável'
+                    ),
+                    yaxis_categoryorder='total ascending',
+                    yaxis_title='',
+                    xaxis_title='Percentual de viagens atrasadas na linha',
+                    )
+    fig.update_traces(textposition='outside')
+    fig.update_traces(hovertemplate='%{y}<br>%{x}% de ocorrência de atraso')
+    
+    fig.show()
+    
+    return None
---
+++ preprocessing.py
@@ -0,0 +1,161 @@
+import pandas as pd
+pd.options.mode.chained_assignment = None
+
+def load_data(url: str, dt_cols: list) -> pd.DataFrame:
+    '''Importa o dataset do endereço especificado e converte as colunas para o tipo datetime.
+       Args: 
+            url: endereço (str) do .csv
+            dt_cols: lista de colunas para serem convertidas para datetime
+        Return:
+            pandas DataFrame 
+    '''
+    df = pd.read_csv(url, index_col=0) # index_col=0 to ignore unnamed column
+    for col in dt_cols:
+        df[col] = pd.to_datetime(df[col])
+    
+    return df
+
+def get_unperformed_frequency(df: pd.DataFrame, by="hour") -> pd.DataFrame:
+    ''' Gera um DataFrame do Pandas contendo a frequência de viagens não realizadas
+        por hora do dia ou por dia da semana. 
+        Args:
+            df: DataFrame do pandas que vai ser usado como base.
+            by: Pode ser "hour" (hora do dia) ou "weekday" (dia da semana). 
+        Return:
+            Pandas DataFrame com informação de frequência de acordo com a separação indicada. 
+    '''
+    if by == "hour":
+        frequency_df = df.groupby(df['hora_prevista'].dt.hour).count()
+        frequency_df = frequency_df.iloc[:, :1].reset_index()
+        frequency_df.columns = ['Hora prevista', 'Viagens não realizadas']
+    elif by == "weekday":
+        df['dia_da_semana'] = unperformed_travels['hora_prevista'].dt.weekday
+        frequency_df = df.groupby(df['dia_da_semana']).count()
+        frequency_df = frequency_df.iloc[:, :1].reset_index().replace(weekday_map)
+        frequency_df.columns = ['Dia da semana', 'Viagens não realizadas']
+    else:
+        raise AssertionError("Modo do parâmetro 'by' admite apenas 'hour' ou 'weekday'")
+
+    return frequency_df
+
+def get_top_n_unperformed(df: pd.DataFrame, n: int) -> pd.DataFrame:
+    ''' Gera um pandas DataFrame com as 'n' linhas que mais frequentemente não cumpriram suas viagens.
+        Args: 
+            df: DataFrame do pandas para ser usado como base. 
+            n: Inteiro. Número de itens para serem mostrados. Ex: n=10 mostra apenas
+            as 10 linhas que mais não cumpriram suas viagens.
+        Return:
+            DataFrame do pandas com: nome da linha, empresa e quantidade de viagens não realizadas. 
+    '''
+    top_unperformed = df.groupby(['nome_linha', 'apelido_empresa'], as_index=False)
+    top_unperformed = top_unperformed.size().rename(columns={'size': 'viagens não realizadas'})
+    top_unperformed = top_unperformed.sort_values(by=['viagens não realizadas']).iloc[-n:, :]
+    return top_unperformed
+
+
+def generate_delay_status_column(df: pd.DataFrame) -> pd.DataFrame:
+    ''' Cria uma coluna no DataFrame para indicar com mais rapidez se a viagem foi 
+        atrasada, adiantada ou pontual. 
+        Args:
+            df: DataFrame do pandas a ser usada como base.
+        Return:
+            O mesmo dataframe da entrada, mas com uma coluna extra de status de atraso. 
+    '''
+    df.loc[(df['hora_prevista'] < df['hora_realizada']), 'status_de_atraso'] = 'Atrasado'
+    df.loc[(df['hora_prevista'] > df['hora_realizada']), 'status_de_atraso'] = 'Adiantado'
+    df.loc[(df['hora_prevista'] == df['hora_realizada']), 'status_de_atraso'] = 'Pontual'
+
+    return df
+
+def calculate_delay(df: pd.DataFrame) -> pd.DataFrame:
+    ''' Calcula o tempo de atraso de cada linha do DataFrame com uma coluna extra 
+        contendo o valor em minutos desse atraso. Adiantamentos foram considerados como
+        atrasos negativos. 
+        Args:
+            df: DataFrame do Pandas usado como base.
+        Return:
+            O mesmo DataFrame da entrada, mas com a coluna extra do tempo de atraso. 
+    '''
+    # *-1 since it's a negative delay (an advancement)
+    df.loc[(df['status_de_atraso'] == 'Adiantado'), 'tempo_de_atraso'] = (df['hora_prevista'] - df['hora_realizada']).dt.seconds / 60 * -1
+    df.loc[(df['status_de_atraso'] == 'Atrasado'), 'tempo_de_atraso'] = (df['hora_realizada'] - df['hora_prevista']).dt.seconds / 60
+    df.loc[(df['status_de_atraso'] == 'Pontual'), 'tempo_de_atraso'] = 0
+
+    return df
+
+def fix_outliers_delays(df: pd.DataFrame):
+    ''' Corrige os atrasos e adiantamentos extremamente discrepantes causados por horários
+        próximos da meia noite. 
+        Args: 
+            df: DataFrame do pandas com coluna 'tempo_de_atraso' a ser corrigida. 
+        Return:
+            DataFrame do Pandas corrigida.
+    '''
+    df.loc[(df['tempo_de_atraso'] > 1000), 'status_de_atraso'] = 'Adiantado'
+    df.loc[(df['tempo_de_atraso'] > 1000), 'tempo_de_atraso'] = df['tempo_de_atraso'] - 1400
+    df.loc[(df['tempo_de_atraso'] < -1000), 'status_de_atraso'] = 'Adiantado'
+    df.loc[(df['tempo_de_atraso'] < -1000), 'tempo_de_atraso'] = df['tempo_de_atraso'] + 1400
+    return df
+
+def get_delay_info(df: pd.DataFrame):
+    ''' Gera um DataFrame do Pandas com informações sobre atraso. 
+        Status do atraso: 'adiantado', 'atrasado', ou 'pontual'
+        Frequência: frequência de ocorrência de cada status.
+        Total: total de viagens
+        Percentual: percentual de cada um dos status
+
+        Args:
+            df: DataFrame a ser usado como base.
+        Return:
+            DataFrame com informações de atraso por empresa e por situação de atraso.
+    '''
+    df = performed_travels.groupby(['apelido_empresa', 'status_de_atraso']).count().reset_index()
+    df = df.iloc[:, :3]
+    df.columns = ['Empresa', 'Status', 'Frequência']
+    for index, company in enumerate(['Cidade de Maceió', 'Real Alagoas', 'São Francisco', 'Veleiro']):
+        df.loc[(df['Empresa'] == company), 'Total'] = df.groupby(['Empresa']).sum().iloc[index, 0]
+    df['Percentual'] = (100*df['Frequência']/df['Total']).round(2)
+    return df
+
+
+def get_delay_avg_info(df: pd.DataFrame, by="hour") -> pd.DataFrame:
+    ''' Gera um dataframe com informações de atraso por hora do dia ou por dia da semana.
+        Args:
+            df: DataFrame com informações a serem visualizadas.
+            by: pode ser "hour" (por hora do dia) ou "weekday" (por dia da semana)
+    '''
+    if by == "hour":
+        delay_info = df.groupby(df['hora_realizada'].dt.hour).mean().reset_index()
+        delay_info = delay_info.loc[:, ['hora_realizada', 'tempo_de_atraso']]
+        delay_info.columns = ['Hora da viagem', 'Atraso médio']
+        delay_info['Atraso médio'] = delay_info['Atraso médio'].round(2) 
+    elif by == "weekday":
+        df['dia_da_semana'] = df['hora_prevista'].dt.weekday
+        delay_info = df.groupby(df['dia_da_semana']).mean()
+        delay_info = delay_info.iloc[:, -1:].reset_index().replace(weekday_map)
+        delay_info.columns = ['Dia da semana', 'Atraso médio']
+        delay_info['Atraso médio'] = delay_info['Atraso médio'].round(2) 
+    else:
+        raise AssertionError("Modo do parâmetro 'by' admite apenas 'hour' ou 'weekday'")
+    
+    return delay_info
+
+
+def get_top_n_delayed(df: pd.DataFrame, n:int) -> pd.DataFrame:
+    ''' Gera um pandas DataFrame com as 'n' linhas que mais atrasaram.
+        Args: 
+            df: DataFrame do pandas para ser usado como base. 
+            n: Inteiro. Número de itens para serem mostrados. Ex: n=10 mostra apenas
+            as 10 linhas que mais atrasaram.
+        Return:
+            DataFrame do pandas com: nome da linha, empresa, frequência de atrasos,
+            percentual de viagens com atraso da linha
+    '''
+    number_of_travels = df.groupby(['nome_linha', 'apelido_empresa'], as_index=False).size().sort_values(by=['size'])
+    delayed_only = df[df['status_de_atraso'] == 'Atrasado']
+    top_delayed = delayed_only.groupby(['nome_linha', 'apelido_empresa'], as_index=False).size()
+    top_delayed = top_delayed.rename(columns={'size': 'Frequência de atrasos'})
+    top_delayed['Percentual de viagens com atraso'] = (100*top_delayed['Frequência de atrasos']/number_of_travels['size']).round(1)
+    top_delayed = top_delayed.sort_values(by=['Percentual de viagens com atraso']).iloc[-n:, :]
+    
+    return top_delayed
